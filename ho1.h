9f31db0d51cb864fe6171e6e67044dc2af10219c
Morten Ledum <morten.ledum@gmail.com>
2015-01-17 23:49:56 +0100

    Test change

--- ho1.h
+++ ho1.h
@@ -7,7 +7,7 @@ class HO1 : public Orbital
 public:
     HO1()  : Orbital() {}
     double eval(double x,double a);
-    double laplacian(double x,double a);
+    double laplacian(double x,double a); //
 };
 
 #endif // HO1_H
